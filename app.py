64ea916a72c605384f7d5e6bb2e58326d9817218
masete <nicholasmasete72@gmail.com>
2023-02-24 09:44:21 +0300

    importations

---
+++ app.py
@@ -0,0 +1,17 @@
+import pickle
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+with open("model.pk", "rb") as f:
+    model = pickle.load(f)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
