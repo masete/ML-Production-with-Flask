1ff907ad99d8b79d8f9abe0c108826744396c8d5
masete <nicholasmasete72@gmail.com>
2023-02-25 10:36:43 +0300

    convert the output back to json

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pickle
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import jsonencoder
+import json
 
 app = Flask(__name__)
 with open("model.pkl", "rb") as f:
@@ -9,9 +10,10 @@ with open("model.pkl", "rb") as f:
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-    output = prediction[0]
+    x = request.get_json(force=True)
+    data = json.dumps(x, cls=jsonencoder.NpEncoder)
+    prediction = model.predict([np.array(list(data))])
+    output = json.dumps(prediction[0])
     return jsonify(output)
 
 if __name__ == "__main__":
