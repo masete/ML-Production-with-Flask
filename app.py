4b54374839ad1de97ca9e6205ff075530d0b4551
masete <nicholasmasete72@gmail.com>
2023-02-26 13:11:41 +0300

    understanding round type

--- app.py
+++ app.py
@@ -20,5 +20,16 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
+    
+    if prediction==0:
+        return render_template('index.html',
+                               prediction_text='Low chances of patient having diabetes'.format(prediction),
+                               )
+    else:
+        return render_template('index.html',
+                               prediction_text='High chances of patient having diabetes'.format(prediction),
+                              )
+
+
 if __name__ == "__main__":
     app.run(debug=True)
