f1219d24d98c704358f84b282d0e8d6974bc2188
masete <nicholasmasete72@gmail.com>
2023-03-02 20:32:49 +0300

    new pickle model

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import pickle
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-import jsonencoder
-import json
 
 app = Flask(__name__)
-with open("model_file.pkl", "rb") as f:
+with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 
 @app.route('/')
@@ -14,11 +12,6 @@ def home():
 
 @app.route("/predict", methods=["POST", "GET"])
 def predict():
-    # data = request.get_json(force=True)
-    # data = json.dumps(x, cls=jsonencoder.NpEncoder)
-    # prediction = model.predict([np.array(list(data.values()))])
-    # output = prediction[0]
-    # return jsonify(output)
 
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
