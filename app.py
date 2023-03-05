20f123ba058bfa45cf396cee90c863c965a45df7
masete <nicholasmasete72@gmail.com>
2023-03-05 14:52:55 +0300

    refactoring for react frontend

--- app.py
+++ app.py
@@ -12,20 +12,38 @@ def home():
 
 @app.route("/predict", methods=["POST", "GET"])
 def predict():
+    try: 
+        values = request.json
+        data = [val for val in values.values()]
+        # int_features = [float(x) for x in request.form.values()]
+        final_features = [np.array(data).reshape(1, -1)]
+        prediction = model.predict(final_features)
+        types = { 0: "Low chances of the startup being success", 1: "High chances that the startup will be successful "}
 
-    int_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-
+        response = jsonify({
+            "statusCode": 200,
+            "status": "Prediction made",
+            "result": "The type of iris plant is: " + types[prediction[0]]
+            })
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
     
-    if prediction==0:
-        return render_template('index.html',
-                               prediction_text='Low chances of the startup being success'.format(prediction),
-                               )
-    else:
-        return render_template('index.html',
-                               prediction_text='High chances that the startup will be successful'.format(prediction),
-                              )
+    except Exception as error:
+        return jsonify({
+            "statusCode": 500,
+            "status": "Could not make prediction",
+            "error": str(error)
+        })
+    
+    
+    # if prediction==0:
+    #     return render_template('index.html',
+    #                            prediction_text='Low chances of the startup being success'.format(prediction),
+    #                            )
+    # else:
+    #     return render_template('index.html',
+    #                            prediction_text='High chances that the startup will be successful'.format(prediction),
+    #                           )
 
 
 if __name__ == "__main__":
