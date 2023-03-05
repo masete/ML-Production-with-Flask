b1bf74103a97832ce1979ade4152a3c5047d3c10
masete <nicholasmasete72@gmail.com>
2023-03-05 15:26:37 +0300

    refactored code

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
+# from flask_restplus import Api, Resource, fields
 import pickle
 import numpy as np
 from flask import Flask, request, jsonify, render_template, make_response
-from flask_restplus import Api, Resource, fields
 
 app = Flask(__name__)
 with open("model.pkl", "rb") as f:
@@ -13,7 +13,7 @@ def home():
 
 @app.route("/predict", methods=["POST", "GET"])
 def predict():
-    class MainClass(Resource):
+    # class MainClass(Resource):
 
         def options(self):
             response = make_response()
@@ -22,29 +22,31 @@ def predict():
             response.headers.add('Access-Control-Allow-Methods', "*")
             return response
         
-        try: 
-            values = request.json
-            data = [val for val in values.values()]
-            # int_features = [float(x) for x in request.form.values()]
-            final_features = [np.array(data).reshape(1, -1)]
-            prediction = model.predict(final_features)
-            types = { 0: "Low chances of the startup being success", 1: "High chances that the startup will be successful "}
+        def post(self):
+        
+            try: 
+                values = request.json
+                data = [val for val in values.values()]
+                # int_features = [float(x) for x in request.form.values()]
+                final_features = [np.array(data).reshape(1, -1)]
+                prediction = model.predict(final_features)
+                types = { 0: "Low chances of the startup being success", 1: "High chances that the startup will be successful "}
 
-            response = jsonify({
-                "statusCode": 200,
-                "status": "Prediction made",
-                "result": "The type of iris plant is: " + types[prediction[0]]
+                response = jsonify({
+                    "statusCode": 200,
+                    "status": "Prediction made",
+                    "result": "The type of iris plant is: " + types[prediction[0]]
+                    })
+                response.headers.add('Access-Control-Allow-Origin', '*')
+                return print(response)
+                
+            except Exception as error:
+                return jsonify({
+                    "statusCode": 500,
+                    "status": "Could not make prediction",
+                    "error": str(error)
                 })
-            response.headers.add('Access-Control-Allow-Origin', '*')
-            return response
-        
-        except Exception as error:
-            return jsonify({
-                "statusCode": 500,
-                "status": "Could not make prediction",
-                "error": str(error)
-            })
-        
+            
     
     # if prediction==0:
     #     return render_template('index.html',
