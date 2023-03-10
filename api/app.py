009fb26ede380ac32be92142aa904a2651dfc6ad
masete <nicholasmasete72@gmail.com>
2023-03-10 13:32:17 +0300

    feat:request to json

--- api/app.py
+++ api/app.py
@@ -1,64 +1,118 @@
 from flask import Flask, request, jsonify, render_template, make_response
-from flask_cors import CORS, cross_origin
+# from flask_cors import CORS, cross_origin
 import pickle
 import numpy as np
-from flask import Flask, request, jsonify, render_template, make_response
 import json
 
 ACL_ORIGIN = 'Access-Control-Allow-Origin'
 
 app = Flask(__name__)
 # CORS(app)
-cors = CORS(app)
+# cors = CORS(app)
 
-@app.after_request
-def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  response.headers.add('Access-Control-Allow-Credentials', 'true')
-  return 
+# @app.after_request
+# def after_request(response):
+#   response.headers.add('Access-Control-Allow-Origin', '*')
+#   response.headers.add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+#   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+#   response.headers.add('Access-Control-Allow-Credentials', 'true')
+#   return 
 
 with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route("/predict_series_A", methods=["GET", "POST"])
 def predictSeriesA():
-    try: 
 
-        values = request.json
-        data = [val for val in values.values()]
+    try:
+        if request.method == "POST":
+            print("request =", request.data)
+            form_values = request.json
+            print("form_values =", form_values)
+            column_names = ["Last_funding_round_raised_amount", 
+                            "age_of_company",
+                            "Amount_of_the_last_funding_type",
+                            "Companies_Information_Level_of_Completeness",
+                            "Stage_DA_Classified_Early",
+                            "number_of_founders",
+                            "number_of_bussiness_categories",
+                            "number_of_market_countires",
+                            "Female_Co_Founder",
+                            "Average_time_of_rounds",
+                            "number_of_investors",
+                            "Sector_Information_Technology",
+                            "Business_model_B2C"]
+
+            for key in form_values:
+                form_values[key] = form_values[key].strip()
+            input_data = np.asarray([float(form_values[i]) for i in column_names]).reshape(
+                1, -1
+            )
+            print("input_data =", input_data)
+            prediction = model.predict(input_data)
+            print(prediction)
+            # form_values = request.form.to_dict()
+            # values = request.json
+            # data = [val for val in values.values()]
         # int_features = [float(x) for x in request.form.values()]
-        final_features = [np.array(data).reshape(1, -1)]
-        # data=json.dumps(final_features)
-        prediction = model.predict(final_features)
+        # final_features = [np.array(int_features)]
+        # print(final_features)
+            # data=json.dumps(final_features)
+        # prediction = model.predict(final_features)
+                  
+
+        # if prediction==0:
+        #     return render_template('index.html',
+        #                     prediction_text='Low chances of the startup being success'.format(prediction),
+        #                     )
+        # else:
+        #     return render_template('index.html',
+        #                     prediction_text='High chances that the startup will be successful'.format(prediction),
+        #                     )
         types = { 0: "Low chances of the startup being success", 1: "High chances that the startup will be successful "}
 
-         
         response = jsonify({
-            "statusCode": 200,
-            "status": "Prediction made",
-            "result": "The are : " + types[prediction[0]]
-            })
-        # if response is not None and response.headers is not None and response.headers.get(ACL_ORIGIN):
-        #     origin_header = response.headers[ACL_ORIGIN]
-        # if response is not None and response.headers is not None and response.headers.get(ACL_ORIGIN):{
-        #         response.headers.add('Access-Control-Allow-Origin', '*')
-        # }
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
-                
-    except Exception as error:
-
-        return jsonify({
-            "statusCode": 500,
-            "status": "Could not make prediction",
-            "error": str(error)
+        "statusCode": 200,
+        "status": "Prediction made",
+        "result": "The are : " + types[prediction[0]]
         })
+        response = json.dumps(prediction)
+        return response
+
+            # response = json.dumps(prediction)
+            # return response
+    except:
+        return json.dumps({"error":"Please Enter Valid Data"})
+    # try: 
+    #     form_values = request.form.to_dict()
+    #     values = request.json
+    #     data = [val for val in values.values()]
+        # int_features = [float(x) for x in request.form.values()]
+        # final_features = [np.array(data).reshape(1, -1)]
+        # data=json.dumps(final_features)
+        # prediction = model.predict(final_features)
+        # types = { 0: "Low chances of the startup being success", 1: "High chances that the startup will be successful "}
+
+         
+    #     response = jsonify({
+    #         "statusCode": 200,
+    #         "status": "Prediction made",
+    #         "result": "The are : " + types[prediction[0]]
+    #         })
+       
+    #     return response
+                
+    # except Exception as error:
+
+    #     return jsonify({
+    #         "statusCode": 500,
+    #         "status": "Could not make prediction",
+    #         "error": str(error)
+    #     })
     
         # if prediction==0:
         #     return render_template('index.html',
