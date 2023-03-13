1dadc25811e3839d26b617f1783cdf9ea54ad4e3
masete <nicholasmasete72@gmail.com>
2023-03-13 16:15:27 +0300

    working on deals pie chart component

--- ui/src/scenes/deals/index.js
+++ ui/src/scenes/deals/index.js
@@ -1,7 +1,6 @@
 import { Box, Typography, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
-// import { mockDataTeam } from "../../data/mockData";
 import { yVposts } from "../../data/yearVposts";
 import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
 import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
