5d4279a449e33ec9e02d7516e6be5821ef3847d6
masete <nicholasmasete72@gmail.com>
2023-03-16 16:58:43 +0300

    added the 5th row for our deals list

--- ui/src/scenes/deals/index.js
+++ ui/src/scenes/deals/index.js
@@ -322,6 +322,48 @@ const Dashboard = () => {
             <GeographyChart isDashboard={true} />
           </Box>
         </Box>
+
+        {/* ROW 5 */}
+        <Box
+          gridColumn="span 12"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+        >
+          <Box
+            mt="25px"
+            p="0 30px"
+            display="flex "
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Box>
+              <Typography
+                variant="h5"
+                fontWeight="600"
+                color={colors.grey[100]}
+              >
+                Investment Deals
+              </Typography>
+              <Typography
+                variant="h3"
+                fontWeight="bold"
+                color={colors.greenAccent[500]}
+              >
+                Number of deals by year from 2007 to 2018
+              </Typography>
+            </Box>
+            <Box>
+              <IconButton>
+                <DownloadOutlinedIcon
+                  sx={{ fontSize: "26px", color: colors.greenAccent[500] }}
+                />
+              </IconButton>
+            </Box>
+          </Box>
+          <Box height="250px" m="-20px 0 0 0">
+            <DealsLineChart />
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
