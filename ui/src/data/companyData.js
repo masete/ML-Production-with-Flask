40e2c0b187601cdd89c969b1120bcdba4549173b
masete <nicholasmasete72@gmail.com>
2023-03-23 11:26:12 +0300

    organising data

---
+++ ui/src/data/companyData.js
@@ -0,0 +1,85 @@
+export const companyVCHQ = [
+
+    {
+     country: "SA",
+     amount:	646,
+    },
+    {
+      country: "Nig",
+      amount:	584,
+    },
+    {
+      country: "ken",
+      amount:	487,
+    },
+    {
+      country: "Egy",
+      amount:	260,
+    },
+    {
+      country: "GHA",
+      amount:	135,
+    },
+    {
+      country: "UGA",
+      amount:	62,
+    },
+    {
+      country: "TZ",
+      amount:	25,
+    },
+    {
+      country: "Tun",
+      amount:	25,
+    },
+    {
+      country: "Ang",
+      amount:	23,
+    },
+    {
+      country: "Rwa",
+      amount:	20,
+    },
+    {
+      country: "SUD",
+      amount:	17,
+    },
+    {
+      country: "Cam",
+      amount:	16,
+    },
+    {
+      country: "Sen",
+      amount:	16,
+    },
+    {
+      country: "Ivo",
+      amount:	15,
+    },
+    {
+      country: "Eth",
+      amount:	14,
+    },
+    {
+      country: "Zam",
+      amount:	14,
+    },
+    {
+      country: "Zim",
+      amount:	14,
+    },
+    {
+      country: "Mor",
+      amount:	12,
+    },
+    {
+      country: "USA",
+      amount:	11,
+    },
+    {
+      country: "Mau",
+      amount:	10,
+    },
+   
+   
+    ]
