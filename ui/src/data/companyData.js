aa50c5a5aec9682439bfb7ca8379c467c9b2d200
masete <nicholasmasete72@gmail.com>
2023-03-24 17:41:56 +0300

    structuring data

--- ui/src/data/companyData.js
+++ ui/src/data/companyData.js
@@ -200,522 +200,522 @@ export const companyVCHQ = [
         ,
     ];
     export const CompGeographyData = [
-        {
-          id: "AFG",
-          value: 0,
-        },
+        // {
+        //   id: "AFG",
+        //   value: 0,
+        // },
         {
           id: "AGO",
           value: 230000,
         },
-        {
-          id: "ALB",
-          value: 0,
-        },
-        {
-          id: "ARE",
-          value: 0,
-        },
-        {
-          id: "ARG",
-          value: 0,
-        },
-        {
-          id: "ARM",
-          value: 0,
-        },
-        {
-          id: "ATA",
-          value: 0,
-        },
-        {
-          id: "ATF",
-          value: 0,
-        },
-        {
-          id: "AUT",
-          value: 0,
-        },
-        {
-          id: "AZE",
-          value: 0,
-        },
-        {
-          id: "BDI",
-          value: 0,
-        },
-        {
-          id: "BEL",
-          value: 0,
-        },
-        {
-          id: "BEN",
-          value: 0,
-        },
-        {
-          id: "BFA",
-          value: 0,
-        },
-        {
-          id: "BGD",
-          value: 0,
-        },
-        {
-          id: "BGR",
-          value: 0,
-        },
-        {
-          id: "BHS",
-          value: 0,
-        },
-        {
-          id: "BIH",
-          value: 0,
-        },
-        {
-          id: "BLR",
-          value: 0,
-        },
-        {
-          id: "BLZ",
-          value: 0,
-        },
-        {
-          id: "BOL",
-          value: 0,
-        },
-        {
-          id: "BRN",
-          value: 0,
-        },
-        {
-          id: "BTN",
-          value: 0,
-        },
-        {
-          id: "BWA",
-          value: 0,
-        },
-        {
-          id: "CAF",
-          value: 0,
-        },
-        {
-          id: "CAN",
-          value: 0,
-        },
-        {
-          id: "CHE",
-          value: 0,
-        },
-        {
-          id: "CHL",
-          value: 0,
-        },
-        {
-          id: "CHN",
-          value: 0,
-        },
-        {
-          id: "CIV",
-          value: 0,
-        },
+        // {
+        //   id: "ALB",
+        //   value: 0,
+        // },
+        // {
+        //   id: "ARE",
+        //   value: 0,
+        // },
+        // {
+        //   id: "ARG",
+        //   value: 0,
+        // },
+        // {
+        //   id: "ARM",
+        //   value: 0,
+        // },
+        // {
+        //   id: "ATA",
+        //   value: 0,
+        // },
+        // {
+        //   id: "ATF",
+        //   value: 0,
+        // },
+        // {
+        //   id: "AUT",
+        //   value: 0,
+        // },
+        // {
+        //   id: "AZE",
+        //   value: 0,
+        // },
+        // {
+        //   id: "BDI",
+        //   value: 0,
+        // },
+        // {
+        //   id: "BEL",
+        //   value: 0,
+        // },
+        // {
+        //   id: "BEN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "BFA",
+        //   value: 0,
+        // },
+        // {
+        //   id: "BGD",
+        //   value: 0,
+        // },
+        // {
+        //   id: "BGR",
+        //   value: 0,
+        // },
+        // {
+        //   id: "BHS",
+        //   value: 0,
+        // },
+        // {
+        //   id: "BIH",
+        //   value: 0,
+        // },
+        // {
+        //   id: "BLR",
+        //   value: 0,
+        // },
+        // {
+        //   id: "BLZ",
+        //   value: 0,
+        // },
+        // {
+        //   id: "BOL",
+        //   value: 0,
+        // },
+        // {
+        //   id: "BRN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "BTN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "BWA",
+        //   value: 0,
+        // },
+        // {
+        //   id: "CAF",
+        //   value: 0,
+        // },
+        // {
+        //   id: "CAN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "CHE",
+        //   value: 0,
+        // },
+        // {
+        //   id: "CHL",
+        //   value: 0,
+        // },
+        // {
+        //   id: "CHN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "CIV",
+        //   value: 0,
+        // },
         {
           id: "CMR",
           value: 160000,
         },
-        {
-          id: "COG",
-          value: 0,
-        },
-        {
-          id: "COL",
-          value: 0,
-        },
-        {
-          id: "CRI",
-          value: 0,
-        },
-        {
-          id: "CUB",
-          value: 0,
-        },
-        {
-          id: "-99",
-          value: 0,
-        },
-        {
-          id: "CYP",
-          value: 0,
-        },
-        {
-          id: "CZE",
-          value: 0,
-        },
-        {
-          id: "DEU",
-          value: 0,
-        },
-        {
-          id: "DJI",
-          value: 0,
-        },
-        {
-          id: "DNK",
-          value: 0,
-        },
-        {
-          id: "DOM",
-          value: 0,
-        },
-        {
-          id: "DZA",
-          value: 0,
-        },
-        {
-          id: "ECU",
-          value: 0,
-        },
+        // {
+        //   id: "COG",
+        //   value: 0,
+        // },
+        // {
+        //   id: "COL",
+        //   value: 0,
+        // },
+        // {
+        //   id: "CRI",
+        //   value: 0,
+        // },
+        // {
+        //   id: "CUB",
+        //   value: 0,
+        // },
+        // {
+        //   id: "-99",
+        //   value: 0,
+        // },
+        // {
+        //   id: "CYP",
+        //   value: 0,
+        // },
+        // {
+        //   id: "CZE",
+        //   value: 0,
+        // },
+        // {
+        //   id: "DEU",
+        //   value: 0,
+        // },
+        // {
+        //   id: "DJI",
+        //   value: 0,
+        // },
+        // {
+        //   id: "DNK",
+        //   value: 0,
+        // },
+        // {
+        //   id: "DOM",
+        //   value: 0,
+        // },
+        // {
+        //   id: "DZA",
+        //   value: 0,
+        // },
+        // {
+        //   id: "ECU",
+        //   value: 0,
+        // },
         {
           id: "EGY",
           value: 2600000,
         },
-        {
-          id: "ERI",
-          value: 0,
-        },
-        {
-          id: "ESP",
-          value: 0,
-        },
-        {
-          id: "EST",
-          value: 0,
-        },
+        // {
+        //   id: "ERI",
+        //   value: 0,
+        // },
+        // {
+        //   id: "ESP",
+        //   value: 0,
+        // },
+        // {
+        //   id: "EST",
+        //   value: 0,
+        // },
         {
           id: "ETH",
-          value: 0,
+          value: 140000,
         },
-        {
-          id: "FIN",
-          value: 0,
-        },
-        {
-          id: "FJI",
-          value: 0,
-        },
-        {
-          id: "FLK",
-          value: 0,
-        },
-        {
-          id: "FRA",
-          value: 0,
-        },
-        {
-          id: "GAB",
-          value: 0,
-        },
-        {
-          id: "GBR",
-          value: 0,
-        },
-        {
-          id: "GEO",
-          value: 0,
-        },
+        // {
+        //   id: "FIN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "FJI",
+        //   value: 0,
+        // },
+        // {
+        //   id: "FLK",
+        //   value: 0,
+        // },
+        // {
+        //   id: "FRA",
+        //   value: 0,
+        // },
+        // {
+        //   id: "GAB",
+        //   value: 0,
+        // },
+        // {
+        //   id: "GBR",
+        //   value: 0,
+        // },
+        // {
+        //   id: "GEO",
+        //   value: 0,
+        // },
         {
           id: "GHA",
           value: 1350000,
         },
-        {
-          id: "GIN",
-          value: 0,
-        },
-        {
-          id: "GMB",
-          value: 0,
-        },
-        {
-          id: "GNB",
-          value: 0,
-        },
-        {
-          id: "GNQ",
-          value: 0,
-        },
-        {
-          id: "GRC",
-          value: 0,
-        },
-        {
-          id: "GTM",
-          value: 0,
-        },
-        {
-          id: "GUY",
-          value: 0,
-        },
-        {
-          id: "HND",
-          value: 0,
-        },
-        {
-          id: "HRV",
-          value: 0,
-        },
-        {
-          id: "HTI",
-          value: 0,
-        },
-        {
-          id: "HUN",
-          value: 0,
-        },
-        {
-          id: "IDN",
-          value: 0,
-        },
-        {
-          id: "IND",
-          value: 0,
-        },
-        {
-          id: "IRL",
-          value: 0,
-        },
-        {
-          id: "IRN",
-          value: 0,
-        },
-        {
-          id: "IRQ",
-          value: 0,
-        },
-        {
-          id: "ISL",
-          value: 0,
-        },
-        {
-          id: "ISR",
-          value: 0,
-        },
-        {
-          id: "ITA",
-          value: 0,
-        },
-        {
-          id: "JAM",
-          value: 0,
-        },
-        {
-          id: "JOR",
-          value: 0,
-        },
-        {
-          id: "JPN",
-          value: 0,
-        },
-        {
-          id: "KAZ",
-          value: 0,
-        },
+        // {
+        //   id: "GIN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "GMB",
+        //   value: 0,
+        // },
+        // {
+        //   id: "GNB",
+        //   value: 0,
+        // },
+        // {
+        //   id: "GNQ",
+        //   value: 0,
+        // },
+        // {
+        //   id: "GRC",
+        //   value: 0,
+        // },
+        // {
+        //   id: "GTM",
+        //   value: 0,
+        // },
+        // {
+        //   id: "GUY",
+        //   value: 0,
+        // },
+        // {
+        //   id: "HND",
+        //   value: 0,
+        // },
+        // {
+        //   id: "HRV",
+        //   value: 0,
+        // },
+        // {
+        //   id: "HTI",
+        //   value: 0,
+        // },
+        // {
+        //   id: "HUN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "IDN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "IND",
+        //   value: 0,
+        // },
+        // {
+        //   id: "IRL",
+        //   value: 0,
+        // },
+        // {
+        //   id: "IRN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "IRQ",
+        //   value: 0,
+        // },
+        // {
+        //   id: "ISL",
+        //   value: 0,
+        // },
+        // {
+        //   id: "ISR",
+        //   value: 0,
+        // },
+        // {
+        //   id: "ITA",
+        //   value: 0,
+        // },
+        // {
+        //   id: "JAM",
+        //   value: 0,
+        // },
+        // {
+        //   id: "JOR",
+        //   value: 0,
+        // },
+        // {
+        //   id: "JPN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "KAZ",
+        //   value: 0,
+        // },
         {
           id: "KEN",
           value: 4870000,
         },
-        {
-          id: "KGZ",
-          value: 0,
-        },
-        {
-          id: "KHM",
-          value: 0,
-        },
-        {
-          id: "OSA",
-          value: 0,
-        },
-        {
-          id: "KWT",
-          value: 0,
-        },
-        {
-          id: "LAO",
-          value: 0,
-        },
-        {
-          id: "LBN",
-          value: 0,
-        },
-        {
-          id: "LBR",
-          value: 0,
-        },
-        {
-          id: "LBY",
-          value: 0,
-        },
-        {
-          id: "LKA",
-          value: 0,
-        },
-        {
-          id: "LSO",
-          value: 0,
-        },
-        {
-          id: "LTU",
-          value: 0,
-        },
-        {
-          id: "LUX",
-          value: 0,
-        },
-        {
-          id: "LVA",
-          value: 0,
-        },
+        // {
+        //   id: "KGZ",
+        //   value: 0,
+        // },
+        // {
+        //   id: "KHM",
+        //   value: 0,
+        // },
+        // {
+        //   id: "OSA",
+        //   value: 0,
+        // },
+        // {
+        //   id: "KWT",
+        //   value: 0,
+        // },
+        // {
+        //   id: "LAO",
+        //   value: 0,
+        // },
+        // {
+        //   id: "LBN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "LBR",
+        //   value: 0,
+        // },
+        // {
+        //   id: "LBY",
+        //   value: 0,
+        // },
+        // {
+        //   id: "LKA",
+        //   value: 0,
+        // },
+        // {
+        //   id: "LSO",
+        //   value: 0,
+        // },
+        // {
+        //   id: "LTU",
+        //   value: 0,
+        // },
+        // {
+        //   id: "LUX",
+        //   value: 0,
+        // },
+        // {
+        //   id: "LVA",
+        //   value: 0,
+        // },
         {
           id: "MAR",
           value: 110000,
         },
-        {
-          id: "MDA",
-          value: 0,
-        },
-        {
-          id: "MDG",
-          value: 0,
-        },
-        {
-          id: "MEX",
-          value: 0,
-        },
-        {
-          id: "MKD",
-          value: 0,
-        },
-        {
-          id: "MLI",
-          value: 0,
-        },
-        {
-          id: "MMR",
-          value: 0,
-        },
-        {
-          id: "MNE",
-          value: 0,
-        },
-        {
-          id: "MNG",
-          value: 0,
-        },
-        {
-          id: "MOZ",
-          value: 0,
-        },
-        {
-          id: "MRT",
-          value: 0,
-        },
-        {
-          id: "MWI",
-          value: 0,
-        },
-        {
-          id: "MYS",
-          value: 0,
-        },
-        {
-          id: "NAM",
-          value: 0,
-        },
-        {
-          id: "NCL",
-          value: 0,
-        },
-        {
-          id: "NER",
-          value: 0,
-        },
-        {
-          id: "NGA",
-          value: 5840000,
-        },
-        {
-          id: "NIC",
-          value: 0,
-        },
-        {
-          id: "NLD",
-          value: 0,
-        },
-        {
-          id: "NOR",
-          value: 0,
-        },
-        {
-          id: "NPL",
-          value: 0,
-        },
-        {
-          id: "NZL",
-          value: 0,
-        },
-        {
-          id: "OMN",
-          value: 0,
-        },
-        {
-          id: "PAK",
-          value: 0,
-        },
-        {
-          id: "PAN",
-          value: 0,
-        },
-        {
-          id: "PER",
-          value: 0,
-        },
-        {
-          id: "PHL",
-          value: 0,
-        },
-        {
-          id: "PNG",
-          value: 0,
-        },
-        {
-          id: "POL",
-          value: 0,
-        },
-        {
-          id: "PRI",
-          value: 0,
-        },
-        {
-          id: "PRT",
-          value: 0,
-        },
-        {
-          id: "PRY",
-          value: 0,
-        },
-        {
-          id: "QAT",
-          value: 0,
-        },
-        {
-          id: "ROU",
-          value: 0,
-        },
-        {
-          id: "RUS",
-          value: 0,
-        },
+        // {
+        //   id: "MDA",
+        //   value: 0,
+        // },
+        // {
+        //   id: "MDG",
+        //   value: 0,
+        // },
+        // {
+        //   id: "MEX",
+        //   value: 0,
+        // },
+        // {
+        //   id: "MKD",
+        //   value: 0,
+        // },
+        // {
+        //   id: "MLI",
+        //   value: 0,
+        // },
+        // {
+        //   id: "MMR",
+        //   value: 0,
+        // },
+        // {
+        //   id: "MNE",
+        //   value: 0,
+        // },
+        // {
+        //   id: "MNG",
+        //   value: 0,
+        // },
+        // {
+        //   id: "MOZ",
+        //   value: 0,
+        // },
+        // {
+        //   id: "MRT",
+        //   value: 0,
+        // },
+        // {
+        //   id: "MWI",
+        //   value: 0,
+        // },
+        // {
+        //   id: "MYS",
+        //   value: 0,
+        // },
+        // {
+        //   id: "NAM",
+        //   value: 0,
+        // },
+        // {
+        //   id: "NCL",
+        //   value: 0,
+        // },
+        // {
+        //   id: "NER",
+        //   value: 0,
+        // },
+        // {
+        //   id: "NGA",
+        //   value: 5840000,
+        // },
+        // {
+        //   id: "NIC",
+        //   value: 0,
+        // },
+        // {
+        //   id: "NLD",
+        //   value: 0,
+        // },
+        // {
+        //   id: "NOR",
+        //   value: 0,
+        // },
+        // {
+        //   id: "NPL",
+        //   value: 0,
+        // },
+        // {
+        //   id: "NZL",
+        //   value: 0,
+        // },
+        // {
+        //   id: "OMN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "PAK",
+        //   value: 0,
+        // },
+        // {
+        //   id: "PAN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "PER",
+        //   value: 0,
+        // },
+        // {
+        //   id: "PHL",
+        //   value: 0,
+        // },
+        // {
+        //   id: "PNG",
+        //   value: 0,
+        // },
+        // {
+        //   id: "POL",
+        //   value: 0,
+        // },
+        // {
+        //   id: "PRI",
+        //   value: 0,
+        // },
+        // {
+        //   id: "PRT",
+        //   value: 0,
+        // },
+        // {
+        //   id: "PRY",
+        //   value: 0,
+        // },
+        // {
+        //   id: "QAT",
+        //   value: 0,
+        // },
+        // {
+        //   id: "ROU",
+        //   value: 0,
+        // },
+        // {
+        //   id: "RUS",
+        //   value: 0,
+        // },
         {
           id: "RWA",
           value: 200000,
@@ -740,50 +740,50 @@ export const companyVCHQ = [
           id: "SEN",
           value: 160000,
         },
-        {
-          id: "SLB",
-          value: 0,
-        },
-        {
-          id: "SLE",
-          value: 0,
-        },
-        {
-          id: "SLV",
-          value: 0,
-        },
-        {
-          id: "ABV",
-          value: 0,
-        },
+        // {
+        //   id: "SLB",
+        //   value: 0,
+        // },
+        // {
+        //   id: "SLE",
+        //   value: 0,
+        // },
+        // {
+        //   id: "SLV",
+        //   value: 0,
+        // },
+        // {
+        //   id: "ABV",
+        //   value: 0,
+        // },
         {
           id: "SOM",
           value: 0,
         },
-        {
-          id: "SRB",
-          value: 0,
-        },
-        {
-          id: "SUR",
-          value: 0,
-        },
-        {
-          id: "SVK",
-          value: 0,
-        },
-        {
-          id: "SVN",
-          value: 0,
-        },
-        {
-          id: "SWZ",
-          value: 0,
-        },
-        {
-          id: "SYR",
-          value: 0,
-        },
+        // {
+        //   id: "SRB",
+        //   value: 0,
+        // },
+        // {
+        //   id: "SUR",
+        //   value: 0,
+        // },
+        // {
+        //   id: "SVK",
+        //   value: 0,
+        // },
+        // {
+        //   id: "SVN",
+        //   value: 0,
+        // },
+        // {
+        //   id: "SWZ",
+        //   value: 0,
+        // },
+        // {
+        //   id: "SYR",
+        //   value: 0,
+        // },
         {
           id: "TCD",
           value: 0,
