156a7ae200cc5630d727049d2fd80d49297b67f5
masete <nicholasmasete72@gmail.com>
2023-05-03 12:13:57 +0300

    getting all investors data

--- api/myapp/views/investors.py
+++ api/myapp/views/investors.py
@@ -17,3 +17,12 @@ def get_one_investor(id):
 	return jsonify({"single investor": results})
 	
 
+@investors.route("/api/v1/investor/")
+def get_all_investor(id):
+	print("masete n")
+
+	
+	c = mysql.db.cursor()
+	c.execute('SELECT * FROM acquisitions')
+	results = c.fetchall()
+	return jsonify({"all investor": results})
