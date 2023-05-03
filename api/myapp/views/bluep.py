212bb1ca113604a1b6f547540ffd3b93ae208b36
masete <nicholasmasete72@gmail.com>
2023-05-03 12:11:59 +0300

    registering investor blueprint

--- api/myapp/views/bluep.py
+++ api/myapp/views/bluep.py
@@ -1,5 +1,6 @@
 # from .hello import hello
 from .acquisitions import acquisitions
 from .predictSeriesA import to_series_A
+from .investors import investors
 
-blueprints = [acquisitions, to_series_A]
+blueprints = [acquisitions, to_series_A, investors]
---
+++ api/myapp/views/investors.py
@@ -0,0 +1,19 @@
+from flask import Blueprint,render_template, jsonify
+
+from ..app import mysql
+
+
+investors = Blueprint("investors",__name__)
+
+
+@investors.route("/api/v1/investor/<int:id>")
+def get_one_investor(id):
+	print("masete n")
+
+	
+	c = mysql.db.cursor()
+	c.execute('SELECT * FROM acquisitions')
+	results = c.fetchone()
+	return jsonify({"single investor": results})
+	
+
