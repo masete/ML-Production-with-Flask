fed77c8150da838c5482cbaaedffc6446ec85498
masete <nicholasmasete72@gmail.com>
2023-05-04 09:59:50 +0300

    register deals blueprint

--- api/myapp/views/bluep.py
+++ api/myapp/views/bluep.py
@@ -2,6 +2,7 @@
 from .acquisitions import acquisitions
 from .predictSeriesA import to_series_A
 from .investors_view import investor
-from .investment_view import deals
+from .deals_view import deals
+# from .investment_view import deals
 
 blueprints = [acquisitions, to_series_A, investor, deals]
--- api/myapp/views/investment_view.py
+++ api/myapp/views/investment_view.py
@@ -1,25 +1,25 @@
-from flask import Blueprint,render_template, jsonify
-import pandas as pd
+# from flask import Blueprint,render_template, jsonify
+# import pandas as pd
 
-from ..app import mysql
-
-
-deals = Blueprint("deals",__name__)
+# from ..app import mysql
 
 
-@deals.route("/api/v1/deals/")
-def get_all_deals():
+# deals = Blueprint("deals",__name__)
+
+
+# @deals.route("/api/v1/deals/")
+# def get_all_deals():
 
 	
-	cursor = mysql.db.cursor()
-	cursor.execute('SELECT * FROM investments')
-	results = cursor.fetchall()
+# 	cursor = mysql.db.cursor()
+# 	cursor.execute('SELECT * FROM investments')
+# 	results = cursor.fetchall()
 
 
 
-	return jsonify({"all deals": results})
+# 	return jsonify({"all deals": results})
 
 
-@deals.route("/api/v1/invanalysis/")
-def get_inv_analysis():
-	df = pd.read_sql("SELECT * FROM investments", con=mysql.db)
+# @deals.route("/api/v1/invanalysis/")
+# def get_inv_analysis():
+# 	df = pd.read_sql("SELECT * FROM investments", con=mysql.db)
