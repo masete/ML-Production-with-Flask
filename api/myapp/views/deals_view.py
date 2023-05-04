c954d073075a1598d2ae76033d5104869fe01cf1
masete <nicholasmasete72@gmail.com>
2023-05-04 08:44:19 +0300

    add year column to investment in our data frame

--- api/myapp/views/deals_view.py
+++ api/myapp/views/deals_view.py
@@ -6,7 +6,13 @@ from ..app import mysql
 
 deals = Blueprint("deals",__name__)
 
+#Add year column from 1_post_date
+def get_year(dt):
+	return dt.year
+
 @deals.route("/api/v1/dealsByYear_linePlot/")
 def get_inv_analysis():
 	df = pd.read_sql("SELECT * FROM investments", con=mysql.db)
+
+	df['year'] = df['1_post_date'].map(get_year)
 
