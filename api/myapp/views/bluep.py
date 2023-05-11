15d172a90089100e334906b378cff2f681d2eb42
masete <nicholasmasete72@gmail.com>
2023-05-11 10:48:17 +0300

    natural oder

--- api/myapp/views/bluep.py
+++ api/myapp/views/bluep.py
@@ -3,6 +3,7 @@ from .acquisitions import acquisitions
 from .predictSeriesA import to_series_A
 from .investors_view import investor
 from .deals_view import deals
+from .dealsList_view import deals_list
 # from .investment_view import deals
 
-blueprints = [acquisitions, to_series_A, investor, deals]
+blueprints = [acquisitions, to_series_A, investor, deals, deals_list]
---
+++ api/myapp/views/dealsList_view.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+import pandas as pd
+
+from ..app import mysql
+
+
+deals_list = Blueprint("deals_list",__name__)
+
