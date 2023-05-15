f9eb45c6dc53a9b14d5e39458b7d7db6104b955c
masete <nicholasmasete72@gmail.com>
2023-05-15 07:57:35 +0300

    Defining an async method called get_db that returns a connection object using aiomysql.connect.

--- api/myapp/app.py
+++ api/myapp/app.py
@@ -14,26 +14,39 @@ class MySQL:
 			self.init_app(app)
 			
 	def init_app(self,app):
-		
-		if not self.db:
-			host = app.config['MYSQL_HOST']
-			port = app.config['MYSQL_PORT']
-			user = app.config['MYSQL_USER']
-			passwd = app.config['MYSQL_PASSWORD']
-			db = app.config['MYSQL_DB']
-			connect_timeout = 5  # set timeout to 5 seconds
 
-			self.db = MySQLdb.connect(
-			host=host, port=port, user=user, passwd=passwd, db=db,
-			connect_timeout=connect_timeout
-		)
+		async def get_db(self):
+		
+			if self.db is None:
+				config = {
+                'host': self.app.config['MYSQL_HOST'],
+                'port': self.app.config['MYSQL_PORT'],
+                'user': self.app.config['MYSQL_USER'],
+                'password': self.app.config['MYSQL_PASSWORD'],
+                'db': self.app.config['MYSQL_DB'],
+                'connect_timeout': 5,
+                'charset': 'utf8mb4'
+            }
+				self.db = await aiomysql.connect(**config)
 			
-			# self.db = MySQLdb.connect(host=host,port=port,
-			# 			  user=user,passwd=passwd,db=db
-			# 			  connect_timeout=connect_timeout)
-			
-			# Add this line to fix "Commands out of sync" error
-			# self.db.autocommit(True)
+			# 	host = app.config['MYSQL_HOST']
+			# 	port = app.config['MYSQL_PORT']
+			# 	user = app.config['MYSQL_USER']
+			# 	passwd = app.config['MYSQL_PASSWORD']
+			# 	db = app.config['MYSQL_DB']
+			# 	connect_timeout = 5  # set timeout to 5 seconds
+
+			# 	self.db = MySQLdb.connect(
+			# 	host=host, port=port, user=user, passwd=passwd, db=db,
+			# 	connect_timeout=connect_timeout
+			# )
+				
+				# self.db = MySQLdb.connect(host=host,port=port,
+				# 			  user=user,passwd=passwd,db=db
+				# 			  connect_timeout=connect_timeout)
+				
+				# Add this line to fix "Commands out of sync" error
+				# self.db.autocommit(True)
 			
 
 
