e932e252a53258f01d9c3bd1efcef75a79e02e2a
masete <nicholasmasete72@gmail.com>
2023-05-16 05:25:48 +0300

    get value of investments

--- api/myapp/views/deals.py
+++ api/myapp/views/deals.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, jsonify, current_app
 import pandas as pd
+import random
 
 deals = Blueprint("deals", __name__)
-mysql = current_app.config['MYSQL']
+mysql = None
 
 @deals.before_app_first_request
 def setup_mysql():
@@ -29,6 +30,48 @@ def get_inv_analysis():
     df = pd.DataFrame(results, columns=columns)
 
     data = df.to_dict(orient='records')
+
+    return jsonify(data)
+
+@deals.route("/api/v1/quarteryValueOfInvestment/")
+def get_valueOfDealsByQuarter():
+        
+    with current_app.app_context():
+        db = mysql.db
+
+    c = db.cursor()
+	
+    query = '''
+		SELECT
+    		CONCAT(YEAR(`when`), '-Q', QUARTER(`when`)) AS quarter,
+    		SUM(amount) DIV 1000000 AS quarterly_value
+		FROM
+    		investments
+		WHERE
+    		`when` BETWEEN '2019-01-01' AND '2023-12-31'
+		GROUP BY
+    		quarter
+	
+	'''
+	
+    df = pd.read_sql_query(query, con=c)
+
+    df['year'] = df['quarter'].str.extract('^(\d{4})')
+    df['quarter'] = df['quarter'].str.extract('^(\d{4}-Q\d)')
+
+    df = df.groupby(['year', 'quarter']).sum().reset_index()
+
+
+    data = df.to_dict(orient='records')
+
+    colors = {}
+
+    for row in data:
+        quarter = row["quarter"]
+        if quarter not in colors:
+			# generate a random color for each unique quarter
+            colors[quarter] = '#' + ''.join(random.choices('0123456789ABCDEF', k=6))
+        row["color"] = colors[quarter]
 
     return jsonify(data)
 
