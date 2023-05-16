732f38e26580dce86729773b65208679507e708c
masete <nicholasmasete72@gmail.com>
2023-05-16 05:11:44 +0300

    implemented deals list view

--- api/myapp/views/deals.py
+++ api/myapp/views/deals.py
@@ -2,10 +2,11 @@ from flask import Blueprint, jsonify, current_app
 import pandas as pd
 
 deals = Blueprint("deals", __name__)
+mysql = current_app.config['MYSQL']
 
 @deals.route("/api/v1/dealsByYear_linePlot/")
 def get_inv_analysis():
-    mysql = current_app.config['MYSQL']
+    
     db = mysql.db
 
     c = db.cursor()
@@ -15,7 +16,27 @@ def get_inv_analysis():
     results = c.fetchall()
 
     # close cursor
-    c.close()
+    # c.close()
+
+    columns = [desc[0] for desc in c.description]  # Get column names from description
+
+    df = pd.DataFrame(results, columns=columns)
+
+    data = df.to_dict(orient='records')
+
+    return jsonify(data)
+
+@deals.route("/api/v1/dealsList/")
+def get_all_dealsList():
+    
+    db = mysql.db
+
+    c = db.cursor()
+    c.execute('''SELECT * FROM investments''')
+    results = c.fetchall()
+
+	# close cursor
+	# c.close()
 
     columns = [desc[0] for desc in c.description]  # Get column names from description
 
