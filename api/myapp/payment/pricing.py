afb9a5fc85bc7cf95b562a2e097add883ccabca9
masete <nicholasmasete72@gmail.com>
2023-06-04 06:37:30 +0300

    using paddle

--- api/myapp/payment/pricing.py
+++ api/myapp/payment/pricing.py
@@ -1,72 +1,70 @@
-from flask import jsonify, request, Blueprint
+from flask import jsonify, request, Blueprint, redirect
 from flask_cors import cross_origin
-import stripe
+import paddle
 
+paddle.api_key = 'YOUR_PADDLE_VENDOR_API_KEY'
 
 pricing = Blueprint("pricing", __name__)
 
 # Step 2: Define the payment endpoint
-def pay_for_deals():
+@pricing.route("/api/v1/pay", methods=['POST'])
+@cross_origin()
+def pay_for_resources():
     try:
         data = request.get_json()
         num_deals = data.get('num_deals')
         unit_cost = 0.001
         total_cost = num_deals * unit_cost
 
-        session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=[{
-                'price_data': {
-                    'currency': 'usd',
-                    'product_data': {
-                        'name': 'Deals Access',
-                    },
-                    'unit_amount': int(total_cost * 100),
-                },
-                'quantity': 1,
+        checkout = paddle.Checkout(
+            title='Deals Access',
+            currency='USD',
+            quantity=1,
+            prices=[{
+                'product_id': 'YOUR_PRODUCT_ID',  # Replace with your Paddle product ID
+                'unit_price': total_cost,
             }],
-            mode='payment',
             success_url='http://yourdomain.com/success',
             cancel_url='http://yourdomain.com/cancel',
         )
 
-        return jsonify({'session_id': session.id})
+        return redirect(checkout.url, code=303)
 
-    except stripe.error.StripeError:
+    except paddle.Error:
         return jsonify({'error': 'Payment request failed.'}), 400
 
 # Step 6: Verify the payment
-@pricing.route('/webhook', methods=['POST'])
-@cross_origin()
-def webhook():
-    payload = request.get_json()
-    event = None
+# @pricing.route('/webhook', methods=['POST'])
+# @cross_origin()
+# def webhook():
+#     payload = request.get_json()
+#     event = None
 
-    try:
-        event = stripe.Event.construct_from(payload, stripe.api_key)
-    except ValueError as e:
-        return jsonify({'error': str(e)}), 400
+#     try:
+#         event = stripe.Event.construct_from(payload, stripe.api_key)
+#     except ValueError as e:
+#         return jsonify({'error': str(e)}), 400
 
-    # Handle the payment succeeded event
-    if event.type == 'checkout.session.completed':
-        session = event.data.object
-        # Retrieve the payment information and mark it as successful in your database
+#     # Handle the payment succeeded event
+#     if event.type == 'checkout.session.completed':
+#         session = event.data.object
+#         # Retrieve the payment information and mark it as successful in your database
 
-    return jsonify({'success': True})
+#     return jsonify({'success': True})
 
 # Step 7: Provide access to the results
-@pricing.route('/results', methods=['GET'])
-@cross_origin()
-def results():
-    # Check if the payment is verified for the user making the request
-    # If the payment is verified, return the requested records
-    # Otherwise, return an error or redirect to the payment endpoint
+# @pricing.route('/results', methods=['GET'])
+# @cross_origin()
+# def results():
+#     # Check if the payment is verified for the user making the request
+#     # If the payment is verified, return the requested records
+#     # Otherwise, return an error or redirect to the payment endpoint
 
-    # Example code for demonstration purposes only
-    # Replace with your own logic to verify the payment status
-    payment_verified = True
+#     # Example code for demonstration purposes only
+#     # Replace with your own logic to verify the payment status
+#     payment_verified = True
 
-    if payment_verified:
-        return jsonify({'data': 'Here are your results.'})
-    else:
-        return jsonify({'error': 'Payment not verified.'}), 401
+#     if payment_verified:
+#         return jsonify({'data': 'Here are your results.'})
+#     else:
+#         return jsonify({'error': 'Payment not verified.'}), 401
--- api/myapp/views/deals.py
+++ api/myapp/views/deals.py
@@ -49,30 +49,30 @@ def get_all_deals():
         print(f"Error: {e}")  # Debug statement
         return jsonify({'error': 'An error occurred'}), 500
 
-@deals.route("/api/v1/get_all_inv/")
-@cross_origin()
-def get_inv_analysis1():
-    try:
+# @deals.route("/api/v1/get_all_inv/")
+# @cross_origin()
+# def get_inv_analysis1():
+#     try:
     
-        with current_app.app_context():
-            db = mysql.db
+#         with current_app.app_context():
+#             db = mysql.db
 
-        c = db.cursor()
-        c.execute('''SELECT * FROM investors_v3''')
-        results = c.fetchall()
+#         c = db.cursor()
+#         c.execute('''SELECT * FROM investors_v3''')
+#         results = c.fetchall()
 
 
-        columns = [desc[0] for desc in c.description]  # Get column names from description
+#         columns = [desc[0] for desc in c.description]  # Get column names from description
 
-        df = pd.DataFrame(results, columns=columns)
+#         df = pd.DataFrame(results, columns=columns)
 
-        data = df.to_dict(orient='records')
+#         data = df.to_dict(orient='records')
 
-        return jsonify(data)
+#         return jsonify(data)
     
-    except Exception as e:
-        print(f"Error: {e}")  # Debug statement
-        return jsonify({'error': 'An error occurred'}), 500
+#     except Exception as e:
+#         print(f"Error: {e}")  # Debug statement
+#         return jsonify({'error': 'An error occurred'}), 500
 
 @deals.route("/api/v1/dealsByYear_linePlot/", methods=['GET'])
 @cross_origin()
